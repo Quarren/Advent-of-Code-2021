cee6c01e19917c261179067d5c86357a00c799ca
parest <pares.thibault@outlook.com>
2021-12-21 21:49:05 +0100

    init

---
+++ src/day1/DepthComparator.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class DepthComparator {
+
+	public DepthComparator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
